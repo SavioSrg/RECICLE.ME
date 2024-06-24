568218e669234c3e46a12e982b9d7f9336f8216b
Lais_lfs <laislara86@gmail.com>
2024-06-24 18:48:42 -0300

    Ajustes js e links

--- login/login-teste.js
+++ login/login-teste.js
@@ -9,14 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
             // Verifica se o email e senha informados correspondem aos dados do usuário cadastrado
             const emailInformado = document.querySelector('.email').value;
             const senhaInformada = document.querySelector('.senha').value;
+            
 
             if(emailInformado === usuario.email && senhaInformada === usuario.senha) {
                 // Se o email e a senha estiverem corretos, redireciona para a página inicial do usuário
-                window.location.href = '../perfis/usuario.html';
+                window.location.href = '../home/home-cliente.html';
             } else {
                 // Se o email ou a senha estiverem incorretos, exibe uma mensagem de erro
                 mostrarMensagemErro("Email ou senha incorretos. Por favor, tente novamente.");
             }
+        } else if(localStorage.getItem('coletores')) {
+            const coletor = JSON.parse(localStorage.getItem('coletores'));
+
+            const emailInformado = document.querySelector('.email').value;
+            const senhaInformada = document.querySelector('.senha').value;
+
+            if(emailInformado === coletor[0].email_coletor && senhaInformada === coletor[0].senha_coletor) {
+                // Se o email e a senha estiverem corretos, redireciona para a página inicial do usuário
+                window.location.href = '../home/home-coletor.html';
+            } else {
+                // Se o email ou a senha estiverem incorretos, exibe uma mensagem de erro
+                mostrarMensagemErro("Email ou senha incorretos. Por favor, tente novamente.");
+            }
+
         } else {
             // Se não houver usuário cadastrado, exibe uma mensagem de erro
             mostrarMensagemErro("Nenhum usuário cadastrado. Por favor, crie uma conta.");
